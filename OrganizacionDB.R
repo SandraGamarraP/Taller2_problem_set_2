b1765157b522c6f799e9afa2bd024a585814a223
SandraGamarraP <s.gamarra@uniandes.edu.co>
2024-04-13 02:33:08 -0500

    Base limpia
    Base limpia

--- OrganizacionDB.R
+++ OrganizacionDB.R
@@ -12,7 +12,7 @@ rm(list = ls())
 
 # Especificar Directorio
 
-# directorio_destino <- "C:/Users/sandr/Documents/GitHub/BIG DATA/"
+directorio_destino <- "C:/Users/sandr/Documents/GitHub/BIG DATA/"
 
 # Cargando librerias y paquetes necesarios 
 
@@ -27,8 +27,7 @@ p_load( tidyverse, # tidy-data
         psych, 
         stargazer, 
         boot, 
-        openxlsx,
-        skimr
+        openxlsx
 )
 
 # Importando los datos 
@@ -38,89 +37,6 @@ train_hogares <- read.csv("data/train_hogares.csv")
 test_personas <- read.csv(("data/test_personas.csv"))
 test_hogares <- read.csv("data/test_hogares.csv")
 sample_submission <- read.csv("data/sample_submission.csv")
-
-# Revisando las variables y la información que contiene la base de datos
-## Hogares
-colnames(train_hogares)
-
-train_hogares  %>% 
-  select(id)  %>%
-  head()
-
-# Según la base del DANE un hogar es clasificado pobre si el “Ingreso percápita de la unidad de gasto con imputación de arriendo a propietarios y usufructuarios” es menor a la Linea de pobreza que le corresponde al hogar.
-table(train_hogares$Pobre)
-
-train_hogares<- train_hogares %>% mutate(Pobre_hand=ifelse(Ingpcug<Lp,1,0))
-table(train_hogares$Pobre,train_hogares$Pobre_hand)
-
-train_hogares<- train_hogares %>% mutate(Pobre_hand_2=ifelse(Ingtotugarr<Lp*Npersug,1,0))
-table(train_hogares$Pobre,train_hogares$Pobre_hand_2)
-
-##Personas
-colnames(train_personas)
-
-train_personas  %>% 
-  select(id, Orden)  %>%
-  head()
-
-# Calculamos el procentaje de inactivos
-new_hogar_variable <- train_personas  %>% 
-  group_by(id) %>%
-  summarize(h_Inactivos=sum(Ina,na.rm=TRUE),
-            h_Pet= sum(Pet, na.rm = TRUE)   ) %>%
-  mutate(h_Inactivosp=h_Inactivos/h_Pet ) %>%
-  ungroup()
-
-new_hogar_variable %>%  
-  head()
-
-# Cálculos de pobreza
-# Comparando variables en la base train y test
-colnames(train_hogares)
-colnames(test_hogares)
-
-colnames(train_personas)
-colnames(test_personas)
-
-# Ver variables con datos missing en la base train_hogares
-datos_miss <- skim(train_hogares) %>% select( skim_variable, n_missing)
-
-Nobs= nrow(train_hogares) 
-Nobs
-
-datos_miss<- datos_miss %>% mutate(p_missing= n_missing/Nobs)
-head(datos_miss)
-
-# Ordenar variables con mayor número de missing
-datos_miss <- datos_miss %>% arrange(-n_missing)
-datos_miss<- datos_miss %>% filter(n_missing!= 0)
-
-#Visualizando la estructura de los missing
-ggplot(datos_miss, aes(x = reorder(skim_variable, +p_missing) , y =  p_missing)) +
-  geom_bar(stat = "identity", fill = "skyblue", color = "black") +
-  coord_flip() +
-  labs(title = "N Missing Per Variable", x = "Var Name", y = "Missings")+ 
-  theme(axis.text = element_text(size = 5)) 
-
-# Ver variables con datos missing en la base train_persona
-datos_miss_per <- skim(train_personas) %>% select( skim_variable, n_missing)
-
-Nobs= nrow(train_personas) 
-Nobs
-
-datos_miss_per <- datos_miss_per %>% mutate(p_missing= n_missing/Nobs)
-head(datos_miss_per)
-
-# Ordenar variables con mayor número de missing
-datos_miss_per <- datos_miss_per %>% arrange(-n_missing)
-datos_miss_per<- datos_miss_per %>% filter(n_missing!= 0)
-
-#Visualizando la estructura de los missing
-ggplot(datos_miss_per, aes(x = reorder(skim_variable, +p_missing) , y =  p_missing)) +
-  geom_bar(stat = "identity", fill = "skyblue", color = "black") +
-  coord_flip() +
-  labs(title = "N Missing Per Variable", x = "Var Name", y = "Missings")+ 
-  theme(axis.text = element_text(size = 5)) 
 
 
 # Unión de las bases 
@@ -142,17 +58,12 @@ for (obj in objetos) {
   
   database <- get(obj)
   
+  # Base train
   
-  # En Edad solo seleccionamos los mayores de 18 años
+  # Edad solo seleccionamos los mayores de 18 años
   
   database <- rename(database, c("edad" = "P6040"))
   database$edad_2 <- database$edad^2
-  
-  # Ocupado 
-  database$ocupado <- ifelse(database$Oc == 1, 1, 0)
-  database$ocupado[database$Oc != 1] <- 0
-  database$ocupado[is.na(database$ocupado)] <- 0
-  
   
   # Género
   
@@ -179,12 +90,6 @@ for (obj in objetos) {
   database$ama_casa[database$P6240 != 4] <- 0
   database$ama_casa[database$P6240 == "."] <- 0
   database$ama_casa[is.na(database$ama_casa)] <- 0
-  
-  # Jefe del hogar
-  database$jefe_hogar <- ifelse(database$P6050 == 1, 1, 0)
-  database$jefe_hogar[database$P6050 != 1] <- 0
-  database$jefe_hogar[database$P6050 == "."] <- 0
-  database$jefe_hogar[is.na(database$jefe_hogar)] <- 0
   
   # Hijos en el hogar
   
@@ -217,10 +122,6 @@ for (obj in objetos) {
   database$superior[database$P6210 == "."] <- 0
   database$superior[is.na(database$superior)] <- 0
   
-  # Arrienda
-  database$arrienda <- ifelse(database$P5090 == 3, 1, 0)
-  database$arrienda[database$P5090 != 3] <- 0
-  database$arrienda[is.na(database$arrienda)] <- 0
   
   # Experiencia trabajo actual
   
@@ -246,15 +147,15 @@ for (obj in objetos) {
   # Imputación de experiencia
   
   database$exp_trabajo_actual <- ifelse(database$edad < 18 & 
-                                          is.na(database$exp_trabajo_actual), 0, 
-                                        database$exp_trabajo_actual)
+                                   is.na(database$exp_trabajo_actual), 0, 
+                                 database$exp_trabajo_actual)
   
   database <- database %>% 
     group_by(id) %>% 
     mutate(mean_experiencia = mean(exp_trabajo_actual, na.rm = TRUE)) %>% 
     ungroup() %>% 
     mutate(exp_trabajo_actual = if_else(is.na(exp_trabajo_actual) & database$edad >= 18, 
-                                        mean_experiencia, database$exp_trabajo_actual))
+                                     mean_experiencia, database$exp_trabajo_actual))
   
   database <- database %>% 
     group_by(id) %>% 
@@ -262,20 +163,20 @@ for (obj in objetos) {
                              exp_trabajo_actual)) %>% 
     ungroup() %>% 
     mutate(experiencia_trab_actual = if_else(is.na(exp_trabajo_actual), 
-                                             variable, database$exp_trabajo_actual))
+                                     variable, database$exp_trabajo_actual))
   
   # Imputación de Horas trabajadas a la semana
   
   database$horas_trab_semana <- ifelse(database$edad < 18 & 
-                                         is.na(database$horas_trab_semana), 0, 
-                                       database$horas_trab_semana)
+                                    is.na(database$horas_trab_semana), 0, 
+                                  database$horas_trab_semana)
   
   database <- database %>% 
     group_by(id) %>% 
     mutate(mean_h = mean(horas_trab_semana, na.rm = TRUE)) %>% 
     ungroup() %>% 
     mutate(horas_trab_semana = if_else(is.na(horas_trab_semana) & database$edad >= 18, 
-                                       mean_h, database$horas_trab_semana))
+                                      mean_h, database$horas_trab_semana))
   
   database <- database %>% 
     group_by(id) %>% 
@@ -283,18 +184,17 @@ for (obj in objetos) {
                              horas_trab_semana)) %>% 
     ungroup() %>% 
     mutate(horas_trab_semana = if_else(is.na(horas_trab_semana), 
-                                       variable, database$horas_trab_semana))
+                                      variable, database$horas_trab_semana))
   
   
   
   database <- subset(database, select = c("id", "Orden", "Clase",
-                                          "ciudad", "edad", "edad_2","ocupado", "mujer", 
-                                          "estudiante", "busca_trabajo", "ama_casa",
-                                          "hijos_hogar", "primaria", "secundaria",
-                                          "media", "superior", "Ingtot",
-                                          "Ingtotug", "exp_trabajo_actual",
-                                          "horas_trab_semana", "Pobre", 
-                                          "numero_personas", "arrienda"))
+                                  "ciudad", "edad", "edad_2", "mujer", 
+                                  "estudiante", "busca_trabajo", "ama_casa",
+                                  "hijos_hogar", "primaria", "secundaria",
+                                  "media", "superior", "Ingtot",
+                                  "Ingtotug", "exp_trabajo_actual",
+                                  "horas_trab_semana", "Pobre", "numero_personas"))
   
   database$num_menores <- as.numeric(database$edad < 18)
   
@@ -303,8 +203,7 @@ for (obj in objetos) {
   database <- database %>% group_by(id) %>%
     summarize(edad = mean(edad),
               edad_2 = mean(edad_2),
-              ocupado = sum(ocupado),
-              mujer = sum(mujer), #mean
+              mujer = mean(mujer),
               estudiante = mean(estudiante),
               busca_trabajo = mean(busca_trabajo),
               ama_casa =mean(ama_casa),
@@ -313,18 +212,16 @@ for (obj in objetos) {
               secundaria = mean(secundaria),
               media = mean(media),
               superior = mean(superior),
-              Ingtot = sum(Ingtot), #Se suma porque es por individuo
-              Ingtotug = mean(Ingtotug), # se promedia porque es por hogar
+              Ingtot = sum(Ingtot),
+              Ingtotug = mean(Ingtotug),
               exp_trabajo_actual = mean(exp_trabajo_actual),
               horas_trab_semana = mean(horas_trab_semana),
               Pobre = mean(Pobre),
               numero_personas = sum(numero_personas),
               num_menores = sum(num_menores),
-              arrienda = mean(arrienda),
               ciudad = first(ciudad))
   
   assign(obj, database)
   rm(database)
   
 }
-############
